b0c7612befdc295fc94a3ee038d735fb06d50026
omar-ahsan <106596821+omar-ahsan@users.noreply.github.com>
2023-05-18 19:58:08 +0500

    Fixed bfs to show complete traversal

--- bfs.py
+++ bfs.py
@@ -1,21 +1,24 @@
-from collections import deque
-
 def breadth_first_search(graph, start, goals):
-    if start not in graph or not all(goal in graph for goal in goals):
-        return []
+    queue = []
+    visited = set()
+    queue.append((start, [start]))  # Store the node and its path
+    visited.add(start)
 
-    queue = deque([(start, [start])])
+    traversal_path = []  # Store the complete traversal path
 
     while queue:
-        node, path = queue.popleft()
+        node, path = queue.pop(0)
+        traversal_path.append(node)  # Store the visited node in the traversal path
 
         if node in goals:
-            return path
+            return traversal_path  # Return the complete traversal path
 
-        neighbors = graph[node]
+        neighbors = graph.neighbors(node)  # Get the neighbors of the current node from the graph
 
         for neighbor in neighbors:
-            if neighbor not in path:
-                queue.append((neighbor, path + [neighbor]))
+            if neighbor not in visited:
+                queue.append((neighbor, path + [neighbor]))  # Update the path
+                visited.add(neighbor)
 
-    return []
+    # If goal node is not found, return None or a custom value
+    return None
